79fbfe878c21da82298f8f6258fdcbe79ae15c2d
Kime <ggopcceop@gmail.com>
2013-06-19 00:06:37 -0300

    enable font anti aliasing

--- src/main/java/me/kime/launcher/Window.java
+++ src/main/java/me/kime/launcher/Window.java
@@ -248,6 +248,10 @@ public class Window extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
+
+        System.setProperty("awt.useSystemAAFontSettings", "on");
+        System.setProperty("swing.aatext", "true");
+        
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
